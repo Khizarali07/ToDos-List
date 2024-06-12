01b90dc0d979787accda3e3a305bbd31041e8547
Khizarali07 <khizarali.cs@gmail.com>
2024-06-12 13:43:05 +0500

    addition

--- server.js
+++ server.js
@@ -7,66 +7,69 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-(async function fun() {
+let db;
+async function initialization() {
   const url =
     "mongodb+srv://khizarali:khizar24@todos.brcftoa.mongodb.net/todos";
   const con = await mongo.MongoClient.connect(url);
-  const db = con.db("todos");
+  db = await con.db("todos");
+}
 
-  await app.post("/givingtask", (req, res) => {
-    const { task, date, complete } = req.body;
-    const data = { task, date, complete };
-    const insert = db.collection("list").insertOne(data);
-  });
+initialization();
 
-  await app.post("/completedone", (req, res) => {
-    const { complete, task } = req.body;
-    const comp = db
-      .collection("list")
-      .updateOne({ task }, { $set: { complete } });
-  });
-  await app.post("/deletedone", (req, res) => {
-    const { date } = req.body;
-    const comp = db.collection("list").deleteOne({ date });
-  });
+app.post("/givingtask", (req, res) => {
+  const { task, date, complete } = req.body;
+  const data = { task, date, complete };
+  const insert = db.collection("list").insertOne(data);
+});
 
-  await app.get("/gettingtasks", async (req, res) => {
-    const alltask = db.collection("list").find();
-    let arr = [];
-    let i = 0;
-    await alltask.forEach((doc) => {
-      arr[i++] = doc;
-      console.log(arr);
-    });
-    await res.status(200).json(arr);
-  });
+app.post("/completedone", (req, res) => {
+  const { complete, task } = req.body;
+  const comp = db
+    .collection("list")
+    .updateOne({ task }, { $set: { complete } });
+});
+app.post("/deletedone", (req, res) => {
+  const { date } = req.body;
+  const comp = db.collection("list").deleteOne({ date });
+});
 
-  await app.get("/showpending", async (req, res) => {
-    const alltask = db.collection("list").find({ complete: "uncomplete" });
-    let arr = [];
-    let i = 0;
-    await alltask.forEach((doc) => {
-      arr[i++] = doc;
-      console.log(arr);
-    });
-    await res.status(200).json(arr);
+app.get("/gettingtasks", async (req, res) => {
+  const alltask = db.collection("list").find();
+  let arr = [];
+  let i = 0;
+  await alltask.forEach((doc) => {
+    arr[i++] = doc;
+    console.log(arr);
   });
+  await res.status(200).json(arr);
+});
 
-  await app.get("/showcomplete", async (req, res) => {
-    const alltask = db.collection("list").find({ complete: "complete" });
-    let arr = [];
-    let i = 0;
-    await alltask.forEach((doc) => {
-      arr[i++] = doc;
-      console.log(arr);
-    });
-    await res.status(200).json(arr);
+app.get("/showpending", async (req, res) => {
+  const alltask = db.collection("list").find({ complete: "uncomplete" });
+  let arr = [];
+  let i = 0;
+  await alltask.forEach((doc) => {
+    arr[i++] = doc;
+    console.log(arr);
   });
+  await res.status(200).json(arr);
+});
 
-  app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "build")));
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+app.get("/showcomplete", async (req, res) => {
+  const alltask = db.collection("list").find({ complete: "complete" });
+  let arr = [];
+  let i = 0;
+  await alltask.forEach((doc) => {
+    arr[i++] = doc;
+    console.log(arr);
   });
+  await res.status(200).json(arr);
+});
 
-  app.listen(3000);
-})();
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "build")));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
